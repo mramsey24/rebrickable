70ba005feff0fedfa68adea1c7cf32246464dfc9
Mark Ramsey <mramsey2624@gmail.com>
2022-12-30 11:54:31 -0500

    Add initial classes

---
+++ src/main/java/com/mvrsoftware/rebrickable/RebrickableApplication.java
@@ -0,0 +1,13 @@
+package com.mvrsoftware.rebrickable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebrickableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebrickableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvrsoftware/rebrickable/RebrickableApplicationTests.java
@@ -0,0 +1,20 @@
+package com.mvrsoftware.rebrickable;
+
+import java.beans.Transient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RebrickableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void firstTest() {
+		System.out.println("Hello, there!");
+	}
+
+}
